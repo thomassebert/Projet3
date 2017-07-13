8f9f8f7f1d23c72be2319e80d768ce4d93609c35
thomassebert <sebert.thomas19@gmail.com>
2017-07-13 14:15:03 +0200

    Add files via upload

---
+++ application/config/prod.php
@@ -0,0 +1,12 @@
+<?php
+
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => 'db688395135.db.1and1.com',
+    'port'     => '3306',
+    'dbname'   => 'db688395135',
+    'user'     => 'dbo688395135',
+    'password' => 'pBT_pPM_qmp_yG5',
+);
