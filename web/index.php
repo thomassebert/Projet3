25d3912c00e4bbfdf296c9bfbcc0b48dc0500a2d
thomassebert <sebert.thomas19@gmail.com>
2017-07-13 14:21:18 +0200

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+require __DIR__.'/../application/config/dev.php';
+require __DIR__.'/../application/app.php';
+require __DIR__.'/../application/routes.php';
+
+$app->run();
